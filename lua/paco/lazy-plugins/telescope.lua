31d5aec4d4173b1807918de9654940a76d6e88b5
Paul Cohen <paul.cohen@seibostudios.se>
2024-01-27 06:50:26 +0100

    Configure telescope.spell_suggest as dropdown

--- lua/paco/lazy-plugins/telescope.lua
+++ lua/paco/lazy-plugins/telescope.lua
@@ -12,14 +12,20 @@ return {
         vim.keymap.set('n', '<leader>sk', telescope.keymaps, { desc = 'Telescope: [S]earch existing [k]eymaps' })
         vim.keymap.set('n', '<leader>sd', telescope.diagnostics, { desc = 'Telescope: [S]earch [D]iagnostics' })
         vim.keymap.set('n', '<leader>sh', telescope.help_tags, { desc = 'Telescope: [S]earch [H]elp' })
-        vim.keymap.set('n', '<leader>ss', telescope.spell_suggest, { desc = 'Telescope: [S]pell [S]uggest' })
+
+        vim.keymap.set('n', '<leader>ss', function()
+            telescope.spell_suggest(require('telescope.themes').get_dropdown {
+                winblend = 20,
+                previewer = false,
+            })
+        end, { desc = 'Telescope: [S]pell [S]uggest' })
 
         vim.keymap.set('n', '<leader>/', function()
           -- You can pass additional configuration to telescope to change theme, layout, etc.
-          telescope.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-            winblend = 10,
-            previewer = false,
-          })
+            telescope.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+                winblend = 20,
+                previewer = false,
+            })
         end, { desc = 'Telescope: [/] Fuzzily search in current buffer' })
 
     end
